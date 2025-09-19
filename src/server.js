148866bba312114418cdafc68ec80860ca1f3dfe
nirapadak <naminopak@gmail.com>
2025-09-19 18:19:16 +0600

    change the sever.js file

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import path from "path";
+// import path from "path";
 import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
@@ -9,7 +9,7 @@ import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
 import { app, server } from "./lib/socket.js";
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 const PORT = ENV.PORT || 3000;
 
@@ -21,14 +21,19 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 // make ready for deployment
-if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// if (ENV.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
-
+//   app.get("*", (_, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//   });
+// }
+app.get('/', (req, res) => {
+  res.status(200).json('API is running....');
+})
+app.get("*", (req, res) => {
+  res.status(404).json({ message: "Route not found" });
+})
 server.listen(PORT, () => {
   console.log("Server running on port: " + PORT);
   connectDB();
